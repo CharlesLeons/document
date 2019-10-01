17ee603f1095fd77132eff7752b0689ede272775
CharlesLeons <51524114+CharlesLeons@users.noreply.github.com>
2019-10-01 19:30:01 +0800

    25 add gesture

--- Front-end/Component/src/Carousel.js
+++ Front-end/Component/src/Carousel.js
@@ -27,7 +27,7 @@ export default class Carousel {
         this[ATTRIBUTE_SYMBOL].root = document.createElement("div");
         this[ATTRIBUTE_SYMBOL].root.classList.add("carousel");
         enableGesture(this[ATTRIBUTE_SYMBOL].root);
-        this[ATTRIBUTE_SYMBOL].root.addEventListener("mousedown", event => event.preventDefault());
+        this.addEvent();
     }
     mounted() {
         this[ATTRIBUTE_SYMBOL].root.style.cssText = this.getAttribute("style");
@@ -35,7 +35,8 @@ export default class Carousel {
         this[ATTRIBUTE_SYMBOL].offsetStartTime = 0;
         this.addIndicator();
         this[ATTRIBUTE_SYMBOL].nextPicTimer = setTimeout(this.nextPic.bind(this), 3000);
-        
+        this[ATTRIBUTE_SYMBOL].startTransform;                      //起始位置
+        this[ATTRIBUTE_SYMBOL].offset = 0;
     }
     unmounted() {
 
@@ -125,10 +126,113 @@ export default class Carousel {
         this[ATTRIBUTE_SYMBOL].nextPicTimer = setTimeout(this.nextPic.bind(this), 3000);
     }
 
-    
-    get children() {
-        return this[PROPERTY_SYMBOL].children;
+    //加入手势
+    addEvent() {
+        this[ATTRIBUTE_SYMBOL].root.addEventListener("mousedown", event => event.preventDefault());
+        this[ATTRIBUTE_SYMBOL].root.addEventListener("mousedown", event => this.mousedown(event));
+        this[ATTRIBUTE_SYMBOL].root.addEventListener("pan", event => this.pan(event));
+        this[ATTRIBUTE_SYMBOL].root.addEventListener("panend", event => this.panend(event));
     }
+
+    mousedown(event) {
+        this[ATTRIBUTE_SYMBOL].tl.pause();
+        let currentTime = Date.now();
+        if (currentTime - this[ATTRIBUTE_SYMBOL].offsetStartTime < 1000) {
+            this[ATTRIBUTE_SYMBOL].offset = 500 - ease((currentTime - this[ATTRIBUTE_SYMBOL].offsetStartTime) / 1000) * 500;
+        } else {
+            this[ATTRIBUTE_SYMBOL].offset = 0;
+        }
+        clearTimeout(this[ATTRIBUTE_SYMBOL].nextPicTimer);
+    }
+    
+    pan(event) {
+        if (event.isVertical) return;
+        
+        let position = this[ATTRIBUTE_SYMBOL].position;
+        let current = this[ATTRIBUTE_SYMBOL].children[position];
+        let nextPosition = (position + 1) % this[ATTRIBUTE_SYMBOL].children.length;
+        let next = this[ATTRIBUTE_SYMBOL].children[nextPosition];
+        let lastPosition = (this[ATTRIBUTE_SYMBOL].children.length + position - 1) % this[ATTRIBUTE_SYMBOL].children.length;
+        let last = this[ATTRIBUTE_SYMBOL].children[lastPosition];
+        
+        last.style.transition = "ease 0s";
+        last.style.transform = `translate(${-500 - 500 * lastPosition + event.dx + this[ATTRIBUTE_SYMBOL].offset}px)`;
+
+        next.style.transition = "ease 0s";
+        next.style.transform = `translate(${500 - 500 * nextPosition + event.dx + this[ATTRIBUTE_SYMBOL].offset}px)`;
+
+        current.style.transition = "ease 0s";
+        current.style.transform = `translate(${ - 500 * position + event.dx + this[ATTRIBUTE_SYMBOL].offset}px)`;
+
+        this[ATTRIBUTE_SYMBOL].position = position;
+    }
+    
+    panend() {
+        let position = this[ATTRIBUTE_SYMBOL].position;
+        let children = this[ATTRIBUTE_SYMBOL].children;
+
+        if (event.isVertical) return;
+        let isLeft;
+        if (event.isFlick && Math.abs(event.dx) > Math.abs(event.dy)) {
+            if (event.dx > 0) {
+                position --;
+                isLeft = true;
+            }
+            if (event.dx < 0) {
+                position ++;
+                isLeft = false;
+            }
+        } else {
+            if (event.dx > 250) {
+                position --;
+                isLeft = true;
+            } else if (event.dx < - 250) {
+                position ++;
+                isLeft = false;
+            } else if (event.dx > 0) {
+                isLeft = false;
+            } else {
+                isLeft = true;
+            }
+        }
+        position = (this[ATTRIBUTE_SYMBOL].children.length + position) % this[ATTRIBUTE_SYMBOL].children.length;
+        
+
+        let current = children[position];
+        let nextPosition = (position + 1) % children.length;
+        let next = children[nextPosition];
+        let lastPosition = (children.length + position - 1) % children.length;
+        let last = children[lastPosition];
+
+        if (!isLeft) {
+            last.style.transition = "";
+        } else {
+            last.style.transition = "ease 0s";
+        }
+        last.style.transform = `translate(${-500 - 500 * lastPosition}px)`;
+
+        if (isLeft) {
+            next.style.transition = "";
+        } else {
+            next.style.transition = "ease 0s";
+        }
+        next.style.transform = `translate(${500 - 500 * nextPosition}px)`;
+
+        current.style.transition = "";
+        current.style.transform = `translate(${- 500 * position}px)`;
+
+        //恢复轮播
+        this[ATTRIBUTE_SYMBOL].nextPicTimer = setTimeout(this.nextPic.bind(this), 3000);
+
+        //conductor
+        for(let child of this[ATTRIBUTE_SYMBOL].indicator.children) {
+            child.style.opacity = "0.3";
+        }
+        this[ATTRIBUTE_SYMBOL].indicator.children[position].style.opacity = 1;
+
+        this[ATTRIBUTE_SYMBOL].position = position;
+    }
+    
 
     getAttribute(name) {
         return this[ATTRIBUTE_SYMBOL][name];
